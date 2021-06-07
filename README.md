caa1bd99a62878df6890b531ee21c92da370516d
brsevero <brsss@ic.ufal.br>
2021-06-06 22:34:02 -0300

    finalização

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # prova_4all
+
+## Desafio 1
+
+1. Acessar o site: https://shopcart-challenge.4all.com/
+1. Selecionar categoria "Doces"
+1.  Adicionar todos os produtos ao carrinho
+1. Selecionar categoria "Todos"
+1. Acessar o carrinho
+1. aumentar a quantidade do produto "Brigadeiro" em 4 unidades
+1. Clicar no botão "Finalizar Compra"
+1. Validar a mensagem "Pedido realizado com sucesso!"
+1. Clicar no botão "Fechar"
+1. Fechar o browser
+
+
+## Desafio 2
+1. Acessar o site: https://shopcart-challenge.4all.com/
+1. Selecionar categoria "Bebidas"
+1. Adicionar todos os produtos ao carrinho
+1. Selecionar categoria "Todos"
+1. Adicionar o produto "Rissole médio" ao carrinho
+1. Acessar o carrinho
+1. aumentar a quantidade do produto "Rissole médio" em 9 unidades
+1. diminuir a quantidade do produto "Rissole médio" em 5 unidades
+1. Validar o valor total dos produtos
+1. Clicar no botão "Finalizar Compra"
+1. Validar a mensagem "Pedido realizado com sucesso!"
+1. Clicar no botão "Fechar"
+1. Fechar o browser
+
+## Observações
+- Os desafios foram realizados em _python 3.9.5_, utilizando as bibliotecas _selenium_ e _unittest_.
+- Todos os desafios obtiveram sucesso seguindo os passos indicados.
+- Tempo de execução dos teste:
+~~~Shell
+
+----------------------------------------------------------------------
+Ran 2 tests in 17.947s
+
+OK
+
+~~~
+- Instalar as dependências e rodar:
+~~~Shell
+
+pip install -r requirements.txt
+python main.py
+
+~~~
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import unittest
 
 class Desafios(unittest.TestCase):
+
     def setUp(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.implicitly_wait(3)
@@ -11,59 +12,59 @@ class Desafios(unittest.TestCase):
         self.driver.maximize_window()
 
     def test_desafio_1(self):
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_css_selector('img.sc-iAyFgw.mBXxg').click()
-        time.sleep(1)        
+        #time.sleep(1)        
         self.driver.find_element_by_id('category-1').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_id('add-product-4-btn').click()
-        time.sleep(1)        
+        #time.sleep(1)        
         self.driver.find_element_by_id('add-product-5-btn').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_class_name('sc-kGXeez.brMovy').click()
-        time.sleep(1)
+        #time.sleep(1)
         for i in range(4):
             self.driver.find_element_by_id('add-product-4-qtd').click()
-            time.sleep(1)
+            #time.sleep(1)
         self.driver.find_element_by_id('finish-checkout-button').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_class_name('close-modal.sc-jqCOkK.ippulb').click()
-        time.sleep(1)
+        #time.sleep(1)
     
     def test_desafio2(self):
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_css_selector('img.sc-iAyFgw.mBXxg').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_id('category-0').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_id('add-product-0-btn').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_id('add-product-1-btn').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_id('add-product-2-btn').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_css_selector('img.sc-iAyFgw.mBXxg').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_id('category-all').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_id('add-product-3-btn').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_class_name('sc-kGXeez.brMovy').click()
-        time.sleep(1)
+        #time.sleep(1)
         for i in range(9):
             self.driver.find_element_by_id('add-product-3-qtd').click()
-            time.sleep(1)
+            #time.sleep(1)
         for i in range(5):
             self.driver.find_element_by_id('remove-product-3-qtd').click()
-            time.sleep(1)
+            #time.sleep(1)
         self.driver.find_element_by_id('finish-checkout-button').click()
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_class_name('close-modal.sc-jqCOkK.ippulb').click()
-        time.sleep(1)
+        #time.sleep(1)
 
     def tearDown(self):
-        self.driver.close
+        self.driver.close()
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
